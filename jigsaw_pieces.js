40af421c730a45b5b010271bd26a0d765f7425ed
blackhawk27 <65427186+blackhawk27@users.noreply.github.com>
2021-11-12 12:40:50 +0100

    Add files via upload

--- jigsaw_pieces.js
+++ jigsaw_pieces.js
@@ -1,1 +1,29 @@
 // Her laver vi brikkerne...
+
+
+class Piece{
+    constructor(xPos, yPos, rowIndex, colIndex){
+        this.xPos = xPos;
+        this.yPos = yPos;
+        this.rowIndex = rowIndex;
+        this.colIndex = colIndex;
+        this.width = puzzleImage.width / columns;
+        this.height = puzzleImage.height / rows;
+    }
+
+    draw(){
+
+        ctx.drawImage(
+            puzzleImage,
+            (puzzleImage.width / columns)* this.colIndex,
+            (puzzleImage.height / rows)* this.rowIndex,
+            (puzzleImage.width/columns),
+            (puzzleImage.height/rows),
+            this.xPos,
+            this.yPos,
+            this.width,
+            this.height
+        );
+
+    }
+}
--- main.js
+++ main.js
@@ -4,8 +4,83 @@ let ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-window.onload = function() {
-    let img = document.getElementById("bugs");
-    ctx.drawImage(img, canvas.width / 2 - img.width / 2, canvas.height / 2 - img.height / 2);
+
+
+
+let puzzleImage = new Image();
+puzzleImage.src = "bugs.jpg";
+
+let mouseIsDown = "false";
+let clickedPiece = {};
+let mousePosition = {};
+let rows = 10;
+let columns = 10;
+let pieces = [];
+
+
+
+puzzelImage.onload = function () {
+  //generate the piece objects at random positions
+  for(let rowNumber = 0; rowNumber < rows; rowNumber++) {
+      for (let columnNumber = 0; columnNumber < columns; columnNumber++) {
+          pieces.push(new Piece(
+              Math.random()*canvas.width*1, // 0.8
+              Math.random()*canvas.height*1, // 0.8
+              rowNumber,
+              columnNumber)
+          );
+      }
+  }
+
+  // draw the pieces 60 times per second
+  setInterval(function() {
+
+      ctx.clearRect(0,0,canvas.width,canvas.height);
+      for(let piece of pieces)
+          piece.draw();
+
+      if(clickedPiece != undefined) {
+          clickedPiece.xPosition = mousePosition.x;
+          clickedPiece.yPosition = mousePosition.y;
+      }
+
+  }, 1000/60);
 }
 
+// update the mouse position
+canvas.addEventListener("mousemove", function (event) {
+  var rect = canvas.getBoundingClientRect();
+  mousePosition = {
+    x: event.clientX - rect.left, // account for border size
+    y: event.clientY - rect.top
+  };
+});
+
+// 
+canvas.addEventListener("mousedown", function (event) {
+
+  if(!mouseIsDown) {
+      for(let piece of pieces) {
+          if(
+              piece.xPosition < mousePosition.x && 
+              piece.yPosition < mousePosition.y &&
+              piece.xPosition + piece.width > mousePosition.x &&
+              piece.yPosition + piece.height > mousePosition.y
+          ) 
+          {
+              clickedPiece = piece;
+          }
+      }
+      mouseIsDown = true;
+  }
+});
+
+canvas.addEventListener("mouseup", function (event) {
+  mouseIsDown = false;
+  clickedPiece = undefined;
+});
+
+document.addEventListener("mouseup", function (event) {
+  mouseIsDown = false;
+  clickedPiece = undefined;
+});
